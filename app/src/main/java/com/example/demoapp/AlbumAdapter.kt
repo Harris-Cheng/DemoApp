33ec042b3983eea0d9e873e0df0c70c7605c5ffd
Harris Cheng <harris_cheng@mtelnet.com>
2020-10-27 23:59:16 +0800

    add BaseAdapter, BaseViewHolder and BaseDiffCallback for inheritance

--- app/src/main/java/com/example/demoapp/AlbumAdapter.kt
+++ app/src/main/java/com/example/demoapp/AlbumAdapter.kt
@@ -5,49 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.demoapp.models.responses.AlbumsResponse
 import kotlinx.android.synthetic.main.item_album.view.*
 
-class AlbumAdapter(callback: DiffUtil.ItemCallback<AlbumsResponse.Result> = AlbumDiffCallback()): ListAdapter<AlbumsResponse.Result, RecyclerView.ViewHolder>(callback) {
+class AlbumAdapter(callback: DiffUtil.ItemCallback<AlbumsResponse.Result> = AlbumDiffCallback()): BaseAdapter<AlbumsResponse.Result>(callback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<AlbumsResponse.Result> {
         return AlbumViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_album, parent, false))
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as? AlbumViewHolder)?.bindView(getItem(position))
-    }
+    inner class AlbumViewHolder(view: View): BaseViewHolder<AlbumsResponse.Result>(view) {
 
-    inner class AlbumViewHolder(view: View): RecyclerView.ViewHolder(view) {
         @SuppressLint("SetTextI18n")
-        fun bindView(album: AlbumsResponse.Result) {
-            itemView.tvArtistName.text = album.artistName.orEmpty()
-            itemView.tvCollectionName.text = album.collectionName.orEmpty()
-            itemView.tvCollectionPrice.text = "${album.currency}$${album.collectionPrice}"
-            itemView.tvCountry.text = album.country.orEmpty()
-            itemView.tvReleaseDate.text = album.releaseDate.orEmpty()
+        override fun bindView(pos: Int, item: AlbumsResponse.Result) {
+            itemView.tvArtistName.text = item.artistName.orEmpty()
+            itemView.tvCollectionName.text = item.collectionName.orEmpty()
+            itemView.tvCollectionPrice.text = "${item.currency}$${item.collectionPrice}"
+            itemView.tvCountry.text = item.country.orEmpty()
+            itemView.tvReleaseDate.text = item.releaseDate.orEmpty()
 
-            Glide.with(itemView).load(album.artworkUrl100.orEmpty()).into(itemView.ivArtist)
+            Glide.with(itemView).load(item.artworkUrl100.orEmpty()).into(itemView.ivArtist)
         }
     }
 
-    class AlbumDiffCallback: DiffUtil.ItemCallback<AlbumsResponse.Result>() {
+    class AlbumDiffCallback: BaseDiffCallback<AlbumsResponse.Result>() {
         override fun areItemsTheSame(
             oldItem: AlbumsResponse.Result,
             newItem: AlbumsResponse.Result
         ): Boolean {
             return oldItem.collectionID == newItem.collectionID
         }
-
-        override fun areContentsTheSame(
-            oldItem: AlbumsResponse.Result,
-            newItem: AlbumsResponse.Result
-        ): Boolean {
-            return false
-        }
-
     }
 }
---
+++ app/src/main/java/com/example/demoapp/BaseAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.demoapp
+
+import android.view.View
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter<T>(
+    callback: DiffUtil.ItemCallback<T> = BaseDiffCallback()
+): ListAdapter<T, BaseAdapter.BaseViewHolder<T>>(callback) {
+
+    final override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
+        holder.bindView(position, getItem(position))
+    }
+
+    final override fun onBindViewHolder(
+        holder: BaseViewHolder<T>,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        holder.bindView(position, getItem(position), payloads)
+    }
+
+    abstract class BaseViewHolder<T>(view: View) : RecyclerView.ViewHolder(view) {
+        abstract fun bindView(pos: Int, item: T)
+        open fun bindView(pos: Int, item: T, payloads: MutableList<Any>) {
+            // by default the payloads are ignored, just go with normal flow to update the whole view holder
+            bindView(pos, item)
+        }
+    }
+
+    open class BaseDiffCallback<T> : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+            return false
+        }
+
+        override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+            return false
+        }
+
+    }
+
+}
